b843715d4c95c3fc1c6fd556161e739d9be43fa8
turpin_l <turpin_l@etna-alternance.net>
2024-10-22 18:24:32 +0200

    feat: export basic response

--- src/shared/basic-response.schema.ts.ts
+++ src/shared/basic-response.schema.ts.ts
@@ -4,3 +4,4 @@ export const basicResponseSchema = z.object({
   message: z.string(),
   data: z.unknown().optional(),
 });
+export type BasicResponse = z.infer<typeof basicResponseSchema>;
---
+++ src/shared/index.ts
@@ -0,0 +1,1 @@
+export * from './basic-response.schema.ts.js';
