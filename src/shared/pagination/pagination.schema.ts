a4e9e95eceeed4701c3612cf6013ac9f6b0d7092
turpin_l <turpin_l@etna-alternance.net>
2024-10-22 18:54:11 +0200

    fix: coerce

--- src/shared/pagination/pagination.schema.ts
+++ src/shared/pagination/pagination.schema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const paginationSchema = z.object({
-  page: z.number().int().min(1).default(1),
-  perPage: z.number().int().min(1).max(100).default(10),
-  total: z.number().int(),
+  page: z.coerce.number().int().min(1).default(1),
+  perPage: z.coerce.number().int().min(1).max(100).default(10),
+  total: z.coerce.number().int(),
 });
 export type Pagination = z.infer<typeof paginationSchema>;
 
