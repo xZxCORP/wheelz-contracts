fce21506900483cc57e3767a98fc191f71eef903
turpin_l <turpin_l@etna-alternance.net>
2024-10-24 15:16:11 +0200

    feat: improve auth schemas

--- src/authentication/schemas/register.ts
+++ src/authentication/schemas/register.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 const registerSchema = z.object({
-  lastname: z.string(),
-  firstname: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  lastname: z.string().min(1),
+  firstname: z.string().min(1),
+  email: z.string().email().min(1),
+  password: z.string().min(6),
 });
 
-export type RegisterSchema = z.infer<typeof registerSchema>;
+export type Register = z.infer<typeof registerSchema>;
 export default registerSchema;
