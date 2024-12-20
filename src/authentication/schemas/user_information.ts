44ef11617c511470cfa3888269e4844d6c21e5b3
tehcuapm <tehcuapm@gmail.com>
2024-12-20 16:41:06 +0100

    fix: typo

--- src/authentication/schemas/user_information.ts
+++ src/authentication/schemas/user_information.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const userInformationSchema = z.object({
   userId: z.number(),
-  role: z.array(z.string())
+  roles: z.array(z.string())
 });
 export type UserInformation = z.infer<typeof userInformationSchema>;
