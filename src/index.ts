0734885386e5125d335ed92fa58e2a5fa9d34c89
tehcuapm <tehcuapm@gmail.com>
2025-01-10 18:43:23 +0100

    fix: remove useless schema index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './authentication/index.js';
 export * from './chain/index.js';
+export * from './role/index.js';
 export * from './shared/index.js';
 export * from './transaction/index.js';
 export * from './user/index.js';
-export * from './role/index.js';
