38f6783b128712c8f3e572d9e0bf44b708fb2733
tehcuapm <tehcuapm@gmail.com>
2025-01-13 15:49:38 +0100

    feat: common response to role routes

--- src/role/contracts.ts
+++ src/role/contracts.ts
@@ -2,19 +2,30 @@ import { initContract } from '@ts-rest/core';
 import { z } from 'zod';
 
 import { healthContract } from '../shared/health/contract.js';
+import { basicResponseSchema } from '../shared/basic-response.schema.ts.js';
 
 const c = initContract();
 
-const contract = c.router({
-  getRoles: {
-    method: 'GET',
-    path: '/role/:id',
-    responses: {
-      200: z.array(z.string()),
+const contract = c.router(
+  {
+    getRoles: {
+      method: 'GET',
+      path: '/role/:id',
+      responses: {
+        200: z.array(z.string()),
+      },
+      summary: 'Get the roles of a user',
     },
-    summary: 'Get the roles of a user',
   },
-});
+  {
+    commonResponses: {
+      404: basicResponseSchema,
+      422: basicResponseSchema,
+      400: basicResponseSchema,
+      500: basicResponseSchema,
+    },
+  }
+);
 
 export const roleContract = c.router({
   contract: contract,
