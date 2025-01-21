5bd4882a512b7f0c4d304ddfd3c4b1b1925a0cbc
tehcuapm <tehcuapm@gmail.com>
2025-01-21 12:32:01 +0100

    feat: add membershipRole to user response

--- src/company/schemas/responses/company-response.schema.ts
+++ src/company/schemas/responses/company-response.schema.ts
@@ -2,9 +2,10 @@ import { z } from 'zod';
 
 import { userSchema } from '../../../user/index.js';
 import { companySchema } from '../company.schema.js';
+import { membershipRoleSchema } from '../membership.schema.js';
 
 export const companyWithUserSchema = companySchema.extend({
-  users: z.array(userSchema),
+  users: z.array(userSchema.extend({membershipRole: membershipRoleSchema.optional()})),
 });
 
 export const companyResponseSchema = z.object({
