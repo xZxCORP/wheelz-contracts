90f1457cfd2f446bcc701831443c48c36ea34087
tehcuapm <tehcuapm@gmail.com>
2025-01-24 14:46:00 +0100

    fix linter

--- src/authentication/contracts.ts
+++ src/authentication/contracts.ts
@@ -1,6 +1,6 @@
 import { initContract } from '@ts-rest/core';
 
-import { companyCreateWithOwnerSchema, companyResponseWithUsersSchema, companyWithUserSchema } from '../company/index.js';
+import { companyCreateWithOwnerSchema, companyResponseWithUsersSchema } from '../company/index.js';
 import { basicResponseSchema } from '../shared/basic-response.schema.ts.js';
 import { healthContract } from '../shared/health/contract.js';
 import { userResponseSchema } from '../user/schemas/responses/users-response.schema.js';
