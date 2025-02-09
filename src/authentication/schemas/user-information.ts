1c899e1dad0815e55b9f75f20106014e73ccbb24
tehcuapm <tehcuapm@gmail.com>
2025-02-09 18:44:03 +0100

    feat: add company id to contract

--- src/authentication/schemas/user-information.ts
+++ src/authentication/schemas/user-information.ts
@@ -3,5 +3,6 @@ import { z } from 'zod';
 export const userInformationSchema = z.object({
   userId: z.number(),
   roles: z.array(z.string()),
+  companyId: z.number().optional(),
 });
 export type UserInformation = z.infer<typeof userInformationSchema>;
