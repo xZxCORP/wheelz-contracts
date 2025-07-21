84aaf61a90c7a3db09099382b9c3ea220230ded6
tehcuapm <tehcuapm@gmail.com>
2025-07-21 16:20:04 +0200

    add partial

--- src/blog/schemas/blog-update.ts
+++ src/blog/schemas/blog-update.ts
@@ -2,10 +2,17 @@ import type { z } from 'zod';
 
 import { blogSchema } from './blog.schema.js';
 
-export const blogUpdateSchema = blogSchema.omit({
-  id: true,
-  createdAt: true,
-  slug: true,
-});
+export const blogUpdateSchema = blogSchema
+  .omit({
+    id: true,
+    createdAt: true,
+    slug: true,
+  })
+  .partial({
+    title: true,
+    content: true,
+    keywords: true,
+    imageUrl: true,
+  });
 
 export type BlogUpdate = z.infer<typeof blogUpdateSchema>;
